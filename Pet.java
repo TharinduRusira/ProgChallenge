0b3c5341ada03f6a41743dbfcb4fa862f3795592
Tharindu Rusira <tharindurusira@gmail.com>
2012-01-19 11:39:55 +0530

    original

---
+++ Pet.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gittest;
+
+/**
+ *
+ * @author M.P.T.R.Kumara 100277E
+ */
+public interface Pet {
+    public void eat();
+    public void makeSound();
+    
+}
