7c75e384074c479959f402f4629ca047f724c7fc
Tharindu Rusira <tharindurusira@gmail.com>
2012-01-19 11:39:47 +0530

    original

---
+++ GitTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gittest;
+
+/**
+ *
+ * @author M.P.T.R.Kumara 100277E
+ */
+public class GitTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            Dog myDog=new Dog();
+            myDog.eat();
+            myDog.makeSound();
+    
+    }
+}
